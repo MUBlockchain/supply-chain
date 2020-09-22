efeadf780fd3def1290ddb099b537a8fb4f17174
Jack Gilcrest <gilcest.jack@gmail.com>
2020-09-22 16:05:58 -0400

    Updates

---
+++ precache-manifest.abdbb54948fd41c237aace461c4bce7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0adf4bd3f4edab5d6befc0b92088d7f5",
+    "url": "/supply-chain/index.html"
+  },
+  {
+    "revision": "80c5c5aa2b3bcd6622e5",
+    "url": "/supply-chain/static/css/main.6e6eed38.chunk.css"
+  },
+  {
+    "revision": "4faa9eec106b7043bee0",
+    "url": "/supply-chain/static/js/2.ebb92bef.chunk.js"
+  },
+  {
+    "revision": "e3dd0de0ecf58d6266316a8b52f015d9",
+    "url": "/supply-chain/static/js/2.ebb92bef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80c5c5aa2b3bcd6622e5",
+    "url": "/supply-chain/static/js/main.244389bb.chunk.js"
+  },
+  {
+    "revision": "e14cbedb0b119a694325",
+    "url": "/supply-chain/static/js/runtime-main.a2819316.js"
+  }
+]);
